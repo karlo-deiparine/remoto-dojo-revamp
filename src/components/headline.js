e0a1bbdec6f74441dbc550a53fd356266dcf307a
Jan Karlo <karlo.deiparine@gmail.com>
2021-09-23 09:13:25 +0800

    adding banner section

--- src/components/headline.js
+++ src/components/headline.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Headline() {
   return (
-    <div className="bg-headline-banner bg-center bg-no-repeat bg-cover">
+    <div className="bg-headline-banner bg-center bg-no-repeat bg-cover h-screen">
 
     </div>
   )
